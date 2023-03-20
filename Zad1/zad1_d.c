d430737e3a616473ba0c9f6e300d9d84a62d38ef
Papis6620 <101109948+Papis6620@users.noreply.github.com>
2023-03-20 10:10:04 +0100

    Final

--- Zad1/zad1_d.c
+++ Zad1/zad1_d.c
@@ -4,29 +4,24 @@
 
 int main(){
     int id;
-    int pok = 0;
-    printf("Proces macierzysty: UID = %d, GID= %d, PID= %d, PGID = %d, PPID = %d \n \n", getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
-    for(int i = 0; i < 3; i++){
+    int i = 0;
+    printf("Pokolnie nr. %d ", i);
+    printf("Proces macierzysty: UID = %d, GID= %d, PID= %d, PGID = %d, PPID = %d \n", getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
+    for(i = 0; i < 3; i++){
         id = fork();
         switch(id){
             case -1:
                 perror("fork error");
                 exit(1);
             case 0:
-<<<<<<< HEAD
-                sleep(i+1);
-                printf("Proces potomny: UID = %d, GID= %d, PID= %d, PGID = %d, PPID = %d \n", getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
-=======
-                pok++;
-                printf("Pokolenie nr. %d\n",pok);
-                printf("Proces potomny: UID = %d, GID= %d, PID= %d, PGID = %d, PPID = %d \n", getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
                 sleep(i);
->>>>>>> bb66df2cd2dc1f216a16262dc87926539735177d
+                printf("Pokolenie nr. %d ", i+1);
+                printf("Proces potomny: UID = %d, GID= %d, PID= %d, PGID = %d, PPID = %d \n", getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
                 break;
             default:
+                sleep(i+1);
                 break;
         }
     }
-    sleep(6);
     return 0;
 }
